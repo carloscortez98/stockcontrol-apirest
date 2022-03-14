ddb06e4114e0e0ee7465484fe66bf96248e55336
carloscortez98 <carloscor998@gmail.com>
2022-03-14 12:50:45 -0300

    Se removio el metodo para eliminar Usuario.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,6 +4,6 @@ const usuariosController = require('../controllers/usuariosController');
 
 // router.post('/', usuariosController.crear);
 router.post('/iniciar', usuariosController.iniciar);
-router.delete('/:id', usuariosController.eliminar);
+// router.delete('/:id', usuariosController.eliminar);
 
 module.exports = router;
